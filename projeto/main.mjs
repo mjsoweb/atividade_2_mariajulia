27150e6b60f54399264c8a5ce4439598ff13017b
mjsoweb <oliveira.maju@yahoo.com.br>
2026-08-23 20:07:26 -0300

    atividade 2

--- projeto/main.mjs
+++ projeto/main.mjs
@@ -2,23 +2,23 @@
 import PJ from './pessoas/PJ.mjs';
 import IEclss, { IEfunc, IEjson } from './objetos/IE.mjs';
 
-console.log("=== INICIANDO TESTES DO SISTEMA ===\n");
+console.log("INICIANDO TESTES DO SISTEMA\n");
 
 // 1. Criando dois objetos da classe PJ (Testando herança e métodos herdados)
 const pj1 = new PJ();
-pj1.setNome("Mariana Souza");          // Método herdado de Pessoa
-pj1.setEmail("mariana@tech.com.br");    // Método herdado de Pessoa
+pj1.setNome("Maria Julia Santos");          // Método herdado de Pessoa
+pj1.setEmail("maria@tech.com.br");    // Método herdado de Pessoa
 pj1.setCNPJ("12.345.678/0001-99");      // Método próprio de PJ
-pj1.setRazaoSocial("Tech Solutions S.A."); // Método próprio de PJ
+pj1.setRazaoSocial("Tech 4 Solutions S.A."); // Método próprio de PJ
 
 const pj2 = new PJ();
-pj2.setNome("Roberto Alves");
-pj2.setEmail("roberto@logistica.com.br");
+pj2.setNome("Gustavo Silva");
+pj2.setEmail("gusta@logistica.com.br");
 pj2.setCNPJ("98.765.432/0001-11");
-pj2.setRazaoSocial("Global Logística LTDA");
+pj2.setRazaoSocial("Mundo da Logística LTDA");
 
 // 2. Testando o operador instanceof (Parte 8)
-console.log("--- Teste de Segurança (instanceof) ---");
+console.log("Teste de Segurança (instanceof)");
 const testeIE = new IEclss("0000", "SP");
 const objetoFalso = { nome: 'Empresa Falsa', email: 'falsa@email.com' };
 
@@ -33,7 +33,7 @@ ieClasse.setPJ(pj1);
 
 // Usando IEfunc()
 const ieFabrica = IEfunc("444555666", "SP");
-ieFabrica.setPJ(pj2); // Usando a segunda PJ (Desafio Extra 2)
+ieFabrica.setPJ(pj2); // Usando a segunda PJ
 
 // Usando IEjson
 IEjson.setNumero("777888999");
@@ -51,7 +51,7 @@ function mostrarIE(ie) {
     
     const empresa = ie.getPJ();
     if (empresa) {
-        console.log("\n=== Pessoa Jurídica ===");
+        console.log("\nPessoa Jurídica");
         console.log(`Nome: ${empresa.getNome()}`);
         console.log(`E-mail: ${empresa.getEmail()}`);
         console.log(`CNPJ: ${empresa.getCNPJ()}`);
